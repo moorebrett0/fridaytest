bb20428d9acbe76e48c33cd6dbd1383b62614ac7
moorebrett0@gmail.com <student@epicodus.com>
2015-03-13 10:38:47 -0700

    add values within method for  and needle, 2 tests passing

--- src/WordCount.php
+++ src/WordCount.php
@@ -5,9 +5,9 @@
     {
         function countRepeats($input_word, $input_string)
         {
-            $haystack = "";
-            $needle = "";
-            $input_word= strtolower($needle);
+            $haystack = "at";
+            $needle = "at";
+            $input_word = strtolower($needle);
             $input_string = strtolower($haystack);
             $result = substr_count($input_string, $input_word);
             return $result;
