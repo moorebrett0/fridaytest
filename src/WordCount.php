2af64a39bcf7ceee834cb582914a5b18070c5b0a
moorebrett0@gmail.com <student@epicodus.com>
2015-03-13 09:45:37 -0700

    add test for one word input

---
+++ src/WordCount.php
@@ -0,0 +1,13 @@
+<?php
+
+
+    class wordCounter
+    {
+        function makeWordCount($input_word)
+        {
+            return str_word_count($input_word);
+        }
+
+    }
+
+?>
---
+++ tests/WordCountTest.php
@@ -0,0 +1,25 @@
+<?php
+    require_once "src/WordCount.php";
+
+    class WordCountTester extends PHPUnit_Framework_TestCase
+    {
+        function test_makeWordCount_oneword()
+        {
+
+    //Arrange
+
+    $test_wordCounter = new WordCounter;
+    $input_word = "a";
+
+    //act
+    $result = $test_wordCounter->makeWordCount($input_word);
+
+    //assert
+    $this->assertEquals("1", $result);
+
+        }
+
+    }
+
+
+?>
